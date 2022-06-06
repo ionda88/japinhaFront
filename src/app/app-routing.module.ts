ae2036b29555258c144818f10273c9570f98153b
Nicolas <nicolas@infolinesystems.com.br>
2022-06-06 09:15:42 -0300

    japinha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {SobreComponent} from "./sobre/sobre.component";
+import {InicioComponent} from "./inicio/inicio.component";
+import {CarrinhoComponent} from "./carrinho/carrinho.component";
+import {LocalizacaoComponent} from "./localizacao/localizacao.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'carrinho', component: CarrinhoComponent },
+  { path: 'localizacao', component: LocalizacaoComponent },
+  { path: '',   redirectTo: '/inicio', pathMatch: 'full' },
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { LocalizacaoComponent } from './localizacao/localizacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SobreComponent,
+    InicioComponent,
+    CarrinhoComponent,
+    LocalizacaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
