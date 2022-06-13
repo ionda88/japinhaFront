b69a7ea6f89eaaa9d6aad3e890aa508494adff99
Nicolas <nicolas@infolinesystems.com.br>
2022-06-13 09:03:34 -0300

    japinha

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Usuario} from "../../entities/usuario";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  modo = "login";
+
+  // emailLogin: any;
+  // senhaLogin:any;
+  //
+  // nomeCadastro:any;
+  // emailCadastro:any;
+  // senha1Cadastro:any;
+  // senha2Cadastro:any;
+  usuario: Usuario = new Usuario();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cadastrar() {
+    if(this.usuario.senha != this.usuario.confirmaSenha) {
+      this.modo = 'cadastro';
+      alert("Senhas não coincidem!");
+    }
+  }
 }
---
+++ src/entities/usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+
+  email = "";
+  nome = "";
+  senha = "";
+  confirmaSenha = "";
+  cpf = "";
+  dtNascimento: Date | null = null;
+  nuTelefone = "";
+}
